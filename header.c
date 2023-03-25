ae634d35b2e648e660196aa90ddddf669fd2a920
Frank Davis <128915704+frankd04@users.noreply.github.com>
2023-03-25 12:20:08 -0500

    Update header.c

--- header.c
+++ header.c
@@ -1,2 +1,18 @@
-// header.c
-// frank edit:
+// header.c for black jack
+
+#include "header.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void cardCounter(){
+  
+}
+void getCard(){
+  
+}
+void scoreCal(){
+  
+}
+void determineWinner(){
+  
+}
