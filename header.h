110cc8c5d193a7ae871e7dc0ba2e5e0fd23a7ae3
Frank Davis <128915704+frankd04@users.noreply.github.com>
2023-03-25 12:39:27 -0500

    Update header.h

--- header.h
+++ header.h
@@ -1,2 +1,14 @@
-// This is a test
-// Take the blue pill or the red pill
+#ifndef HEADER_H
+#define HEADER_H
+  
+typedef struct{
+  int p1, p2, p3, p4;
+  
+  
+void cardCounter();
+void getCard();
+void scoreCal();
+void determineWinner();
+void playerInfo(int p1, int p2, int p3, int p4);
+
+#endif
