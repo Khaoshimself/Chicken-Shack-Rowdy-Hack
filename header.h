e5ffc8a86bfeea34abb1ae08384bcff58139b82c
Frank Davis <128915704+frankd04@users.noreply.github.com>
2023-03-25 12:58:37 -0500

    Update header.h

--- header.h
+++ header.h
@@ -3,12 +3,11 @@
   
 typedef struct{
   int p1, p2, p3, p4;
-  
-  
-void cardCounter();
-void getCard();
-void scoreCal();
-void determineWinner();
+}
+ 
+bool cardCount(int count);
+int dealCard();
+int calScore(int[]);
 void playerInfo(int p1, int p2, int p3, int p4);
 
 #endif
