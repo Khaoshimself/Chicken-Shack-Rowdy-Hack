67964d16795f801faed3330beb5435eb2c1aa7c0
Kyle-Melito <128916046+Kyle-Melito@users.noreply.github.com>
2023-03-25 14:13:16 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,21 +2,25 @@
 //CHIMKEN
 
 #include <stdio.h>
+#include <time.h>
 #include "header.c"
 
 int main(void){
+  //player hands
   int p1[10];
   int p2[10];
   int p3[10];
   int p4[10];
-  int numPlayers;
-  int hitDecision;
-  int p1Count = 0, p2Count = 0, p3Count = 0, p4Count = 0;
-  int p1Score = 0, p2Score = 0, p3Score = 0, p4Score = 0;
-  int card;
+  
+  int numPlayers; //number of players max 4
+  int hitDecision; //hit or stand
+  
+  int p1Count = 0, p2Count = 0, p3Count = 0, p4Count = 0; //Player scores
+  
+  int card; //To hold dealt card
   
   
-  printf("Welcome to Black Jack! \n How many players?");
+  printf("Welcome to Black Jack! \n How many players?"); // welcome statement
   scanf("%d", &numPlayers);
   
   if (numPlayers >= 1). //INITIAL DEALING OF THE CARDS
@@ -61,3 +65,102 @@ int main(void){
     }    
   
 }
+
+int dealcard ()
+{
+  
+  int card = 0;
+  bool available;
+  
+  do {
+    srand(time(0));
+    card = (rand() % 13) + 1;
+    available = cardcount(card)
+    
+  }
+  while (!available)
+  return card;
+}
+
+
+
+int calscore (int array[])
+{
+  
+ int total = 0;
+  
+  for (int i = 0; i < 10; i++)
+  {
+    total =+ array[i];
+  }
+  
+  if (total > 21)
+    total = -1;
+  
+  return total
+  }
+
+bool cardcount (int card)
+{
+ 
+  switch (card)
+  {
+    case 1 : 
+      
+    break;
+      
+    case 2 : 
+      
+    break;
+      
+    case 3 : 
+      
+    break;
+      
+    case 4 : 
+      
+    break;
+      
+    case 5 : 
+      
+    break;
+      
+    case 6 : 
+      
+    break;
+      
+    case 7 : 
+      
+    break;
+      
+    case 8 : 
+      
+    break;
+      
+    case 9 : 
+      
+    break;
+      
+    case 10 : 
+      
+    break;
+      
+    case 11 : 
+      
+    break;
+      
+    case 12 : 
+      
+    break;
+    
+    case 13 :
+      
+    break;
+  }
+ 
+  
+  
+}
+  
+    
+      
