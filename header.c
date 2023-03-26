62c395a313e69a0cce2b63dff8009a9e97007344
Kyle-Melito <128916046+Kyle-Melito@users.noreply.github.com>
2023-03-25 21:18:13 -0500

    Update header.c

--- header.c
+++ header.c
@@ -34,64 +34,133 @@ int calScore (int array[]) {
 bool cardCount (int &card) {
   switch (card) {
     case 1 :       
-      int* pointer = *ace-1;
-      printf("\nYou drew an Ace. Is it 1 or 11");
-             scanf("%d", &card);
+      int* pointer = *ace;
+       if (*pointer > 0)
+      {
+        printf("\nYou drew an Ace. Is it 1 or 11");
+        do 
+        {
+         scanf("%d", &card);
+         if (card != 1 && card != 11)
+           printf("\nPlease enter either 1 or 11");
+        } while (card != 1 && card != 11);
+        *pointer--;
+        return true;
+      }
+    
+             
       
     case 13 :     
-      int *pointer = *king-1;
-      printf("\nYou drew a King");
+      int *pointer = *king;
+      if (*pointer > 0)
+      {
+        printf("\nYou drew a King");
+        *pointer--;
+        card = 10;
+        return true;
+      }
       
     case 2 :     
-      int *pointer = *two-1;
-      printf("\nYou drew a Two);
+      int *pointer = *two;
+      if (*pointer > 0)
+      {
+        printf("\nYou drew a Two");
+        *pointer--;
+        return true;
+      }
              
     case 3 : 
-      int *pointer = *three-1;
-      printf("\nYou drew a Three");
+      int *pointer = *three;
+     if (*pointer > 0)
+      {
+        printf("\nYou drew a Three");
+        *pointer--;
+        return true;
+      }
              
     case 4 : 
-      int *pointer = *four-1;
-      printf("\nYou drew a Four");
+      int *pointer = *four;
+       if (*pointer > 0)
+      {
+        printf("\nYou drew a Four");
+        *pointer--;
+        return true;
+      }
              
     case 5 :
-      int *pointer = *five-1;
-      printf("\nYou drew a Five");
-             
+      int *pointer = *five;
+      if (*pointer > 0)
+      {
+        *pointer--;
+        printf("\nYou drew a Five");
+        return true;
+      }
+      
     case 6 : 
-      int *pointer = *six-1;
-      printf("\nYou drew a Six");
+      int *pointer = *six;
+      if (*pointer > 0)
+      {
+        *pointer--;
+        printf("\nYou drew a Six");
+        return true;
+      }
              
     case 7 : 
-      int *pointer = *seven-1;
-      printf("\nYou drew a Seven");
-             
+      int *pointer = *seven;
+      if (*pointer > 0)
+      {
+        printf("\nYou drew a Seven");
+        *pointer--;
+        return true;
+      }
+      
     case 8 : 
-      int *pointer = *eight-1;
-      printf("\nYou drew a Eight");
-             
+      int *pointer = *eight;
+      if (*pointer > 0)
+      {
+        printf("\nYou drew a Eight");
+        *pointer--;
+        return true;
+      }
+      
     case 9 :
-      int *pointer = *nine-1;
-      printf("You drew a Nine");
-             
+      int *pointer = *nine;
+      if (*pointer > 0)
+      {
+        printf("You drew a Nine");
+        *pointer--;
+        return true;
+      }
+      
     case 10 :
-      int *pointer = *ten-1;
-      printf("You drew a Ten");
+      int *pointer = *ten;
+      if (*pointer > 0)
+      {
+        printf("You drew a Ten");
+        *pointer--;
+        return true;
+      }
              
     case 11 :
-      int *pointer = *jack-1;
-      printf("You drew a Jack");
-      card = 10;
+      int *pointer = *jack;
+      if (*pointer > 0)
+      {
+        *pointer--;
+        printf("You drew a Jack");
+        card = 10;
+        return true;
+      }
              
     case 12 :
       int *pointer = *queen;
-      if (pointer > 0)
+      if (*pointer > 0)
              {
                *pointer--;
                printf("\nYou drew a Queen");
+               card = 10;
+               return true;
              }
-      return
-             
     break;
+    return;
   }
 }
